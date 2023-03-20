f3ca0d47016171c3cf93ef29a00b82e5772d82f5
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2023-03-20 15:50:49 +0000

    remove some java

--- src/main/scala/com/example/LambdaHandler.scala
+++ src/main/scala/com/example/LambdaHandler.scala
@@ -4,16 +4,16 @@ import java.util.{ Map => JavaMap }
 import com.amazonaws.lambda.thirdparty.com.google.gson.GsonBuilder
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
-class LambdaHandler() extends RequestHandler[JavaMap[String, String], String] {
+class     LambdaHandler() extends RequestHandler[JavaMap[String, String], String] {
   val gson = new GsonBuilder().setPrettyPrinting.create
 
   override def handleRequest(event: JavaMap[String, String], context: Context): String = {
     val logger = context.getLogger
 
-    logger.log(s"ENVIRONMENT VARIABLES: ${gson.toJson(System.getenv)}\n")
-    logger.log(s"CONTEXT: ${gson.toJson(context)}\n")
+    logger.log(s"ENVIRONMENT VARIABLES: ")
+    logger.log(s"CONTEXT: $context")
 
-    logger.log(s"EVENT: ${gson.toJson(event)}\n")
+    logger.log(s"EVENT: $event")
 
     "Hello from Scala!"
   }
